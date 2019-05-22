be100b32ca58e8d62c231e437159cba1f2a424f2
yuhuajie <18666713785@163.com>
2019-05-22 19:19:06 +0800

    fix

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import Table from './src/table.vue'
 import TableColumn from './src/table-column'
 
-function install (Vue) {
+function install(Vue) {
     // console.log(Table.name)
     Vue.component(Table.name, Table)
     Vue.component(TableColumn.name, TableColumn)
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue);
+    install(window.Vue)
 }
 
 export default {
